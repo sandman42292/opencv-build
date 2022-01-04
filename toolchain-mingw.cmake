c35b839566daafe5a4ae8284d62016ebc1d47f5f
sandman42292 <sandman42292@gmail.com>
2022-01-04 22:19:27 +0900

    initial commit

---
+++ toolchain-mingw.cmake
@@ -0,0 +1,11 @@
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Windows)
+SET(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc-posix)
+SET(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++-posix)
+SET(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH  /usr/x86_64-w64-mingw32)
